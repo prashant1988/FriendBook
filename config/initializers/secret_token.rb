fb11c2fc0d594452cc40cad70a8eb714d9c65e5c
prashant <prashant1988mishra@gmail.com>
2014-03-29 20:17:25 +0530

    add BootStrap images,js,stylesheet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendbook::Application.config.secret_token = '17d958ae238d147855c6856a47369f351e67eaebea830cdfc925dfb6b2a8ac33d0484861aa4df9c245c56033af29ce1f6485c743fe953badeeae67b5e4dd1090'
