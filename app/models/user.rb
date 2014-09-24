740fd48d6b737e66b970f1897446f9e57450a04a
prashant <prashant1988mishra@gmail.com>
2014-09-24 18:15:14 +0530

    update images with gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,13 @@ class User < ActiveRecord::Base
   def full_name
     first_name + " " +last_name
   end
+  
+  def gravatar_url
+    downcased_stripped_email=email.strip.downcase
+    hash= Digest::MD5.hexdigest(downcased_stripped_email)
+
+    "http://gravatar.com/avatar/#{hash}" 
+  end
 end
+
+
