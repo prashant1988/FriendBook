6803208fdc91b68d312c159ac5c4f544c3e1d25c
prashant <prashant1988mishra@gmail.com>
2014-09-24 13:03:57 +0530

    update test for user module

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a users should enter there first name" do
+    user=User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+  test "a users should enter there last name" do
+    user=User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+  test "a users should enter there profile name" do
+    user=User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+  test "a user should have unique profile name" do 
+    user=User.new
+    user.profile_name=users(:a).profile_name
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+  test "a user profile name should be without sapce" do 
+     user = User.new
+     user.profile_name="My name with sapce"
+     assert !user.save
+     assert !user.errors[:profile_name].empty?
+     assert user.errors[:profile_name].include?("Must be correct format")
+  end 
 end
